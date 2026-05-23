ad12cbf5c621ae54a315bec1da25dbfc2b49bf27
vorobevsvatoslav4-netizen <vorobevsvatoslav4@gmail.com>
2026-05-23 03:17:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { exec } from "node:child_process";
 import pino from "pino";
-import { cloudflare } from "@cloudflare/vite-plugin";
+
 const logger = pino();
 
 const stripAnsi = (str: string) =>
@@ -88,7 +88,7 @@ function watchDependenciesPlugin() {
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd());
   return defineConfig({
-plugins: [react(), cloudflare(), watchDependenciesPlugin()],
+plugins: [react(), watchDependenciesPlugin()],
 build: {   
       minify: true,
       sourcemap: "inline", // Use inline source maps for better error reporting
